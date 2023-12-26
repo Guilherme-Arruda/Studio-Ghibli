238f3753f38cd5209f1a7e93cd67f8d7d69f6771
Guilherme Arruda <arruda.guilherme@outlook.com>
2023-12-26 17:53:09 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 const App = () => {
   return (
     <>
-      <div>
-        <p>Hello</p>
-      </div>
+      <Navbar />
+      <Outlet />
     </>
   );
-};
+}
 
 export default App;
